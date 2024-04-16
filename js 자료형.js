ff0de8a1370e37386575dcdbae98f0f2f748cfca
TH <kareme007@gmail.com>
2024-04-16 18:34:16 +0900

    2024-04-16

---
+++ js 자료형.js
@@ -0,0 +1,17 @@
+//문자형
+let str = "Hello";
+let str2 = 'Hello';
+
+//숫자형
+const num = 0.1 + 0.2;  //부동소수를 사용해서 소수 계산이 다르게 나온다.
+const num2 = (0.1 * 10 + 0.2 * 10) / 10;    //원하는 값 계산 가능
+console.log(num);
+console.log(num2);
+
+//논리형 - true & false
+
+//undefined - 선언만 하고 할당 안하는 경우. 기본값. null이 기본값 아님.
+let x;
+console.log(x);
+let y = null;
+console.log(y);
