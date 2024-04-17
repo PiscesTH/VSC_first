70ab58b8fd947c12067fa80d8e4cd0fe5b71e502
TH <kareme007@gmail.com>
2024-04-17 14:35:05 +0900

    2024-04-17

--- js연산자.js
+++ js연산자.js
@@ -2,7 +2,7 @@
 //비교연산자
 let num = 10;
 let strNum = '10';
-let result = num == strNum; //같은 자료형으로 자동형변환 후 비교 <> !=
+let result = num == strNum; //같은 자료형으로 자동형변환 후 비교 <> != 
 console.log(result);
 let result2 = num === strNum;   //자료형까지 비교 <> !==
 console.log(result2);
